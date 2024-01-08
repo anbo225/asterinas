ad4b31cfcbdf3254126ab8fd7ca4349513e805a4
Ruihan Li <lrh2000@pku.edu.cn>
2024-01-08 23:35:29 +0800

    Implement `(un)register_observer` for TCP/UDP

--- kernel/aster-nix/src/net/socket/ip/stream/mod.rs
+++ kernel/aster-nix/src/net/socket/ip/stream/mod.rs
@@ -293,6 +293,24 @@ impl FileLike for StreamSocket {
     fn as_socket(self: Arc<Self>) -> Option<Arc<dyn Socket>> {
         Some(self)
     }
+
+    fn register_observer(
+        &self,
+        observer: Weak<dyn Observer<IoEvents>>,
+        mask: IoEvents,
+    ) -> Result<()> {
+        self.pollee.register_observer(observer, mask);
+        Ok(())
+    }
+
+    fn unregister_observer(
+        &self,
+        observer: &Weak<dyn Observer<IoEvents>>,
+    ) -> Result<Weak<dyn Observer<IoEvents>>> {
+        self.pollee
+            .unregister_observer(observer)
+            .ok_or_else(|| Error::with_message(Errno::ENOENT, "observer is not registered"))
+    }
 }
 
 impl Socket for StreamSocket {
